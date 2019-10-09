c5faccb6f0b1085f1334f909d8c7fa9bfeadfebe
lizuoyue <li.zuoyue@foxmail.com>
2019-10-09 13:37:43 +0200

    a

---
+++ z_make_bicycle_dataset.py
@@ -0,0 +1,19 @@
+from PIL import Image
+import glob
+import numpy as np
+import os
+import tqdm
+
+target = '../dataset/L2R_Bicycle'
+os.makedirs(target, exist_ok = True)
+for mode in ['train', 'test']:
+	dataset_dir = '../dataset/L2R_Zuoyue' % mode
+	path = dataset_dir + '/*_street_sem2.png'
+	files = glob.glob(path)
+	for file in tqdm.tqdm(files):
+		sem = np.array(Image.open(file))
+		rgb = np.array(Image.open(file.replace('sem2', 'rgb')))
+		bi = np.concatenate([sem, rgb], 1)
+		print(bi.shape)
+		basename = os.path.basename(file)
+		Image.fromarray(bi).save(target + '/' + basename.replace('_street_sem2', ''))
