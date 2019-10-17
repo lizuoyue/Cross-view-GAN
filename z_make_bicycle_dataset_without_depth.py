84ee11dd6c88e92f30d056c0e4e5bfa37a390848
lizuoyue <li.zuoyue@foxmail.com>
2019-10-17 23:22:51 +0800

    a

--- z_make_bicycle_dataset_without_depth.py
+++ z_make_bicycle_dataset_without_depth.py
@@ -5,14 +5,14 @@ import os
 import tqdm
 
 two_dim = np.array([
-	[255, 255],
-	[  0, 127],
-	[127,   0],
-	[  0, 255],
-	[255,   0],
+	[255, 255, 0],
+	[  0, 127, 0],
+	[127,   0, 0],
+	[  0, 255, 0],
+	[255,   0, 0],
 ])
 
-target = '../dataset/L2R_Bicycle_Depth'
+target = '../dataset/L2R_Bicycle_No_Depth'
 os.makedirs(target, exist_ok = True)
 for mode in ['/train', '/test']:
 	os.makedirs(target + mode, exist_ok = True)
@@ -21,12 +21,10 @@ for mode in ['/train', '/test']:
 	files = glob.glob(path)
 	for file in tqdm.tqdm(files):
 		sem = np.array(Image.open(file))
-		dep = np.array(Image.open(file.replace('street_sem_label', 'proj_depth')).convert('L'))
 		rgb = np.array(Image.open(file.replace('sem_label', 'rgb')))
 		info = rgb.copy()
-		info[..., 2] = dep
 		for i in range(5):
-			info[sem == i, :2] = two_dim[i]
+			info[sem == i, :] = two_dim[i]
 		bi = np.concatenate([info, rgb], 1)
 		basename = '/' + os.path.basename(file)
 		Image.fromarray(bi).save(target + mode.replace('test', 'val') + basename.replace('_street_sem_label', ''))
